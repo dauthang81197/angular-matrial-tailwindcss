d41586bf245012a8cb05bdd22c085cbe94b87f54
Đậu Thế Thắng <thangdt@vmodev.com>
2023-03-06 11:18:45 +0700

    [thangdt] update layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { LayoutModule } from './layout/layout.module';
 import { SharedModule } from './shared/shared.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     SharedModule,
     CoreModule,
     LayoutModule,
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LayoutComponent } from './layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
+const LAYOUT_COMPONENTS = [LayoutComponent, HeaderComponent, FooterComponent, SidebarComponent]
 @NgModule({
+  declarations: [...LAYOUT_COMPONENTS],
   imports: [SharedModule, RouterModule],
-  // declarations: [...LAYOUT_COMPONENTS],
 })
 export class LayoutModule {}
